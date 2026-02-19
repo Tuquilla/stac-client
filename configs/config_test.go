714fdb240e89a298c4edc5150cd94709734b4568
Kilian Glaus <kilian.glaus@hotmail.com>
2026-02-19 21:55:48 +0100

    Fix config test

--- configs/config_test.go
+++ configs/config_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestConfig(t *testing.T) {
-	config := ReadConfig("config_fixture.json")
+	config := ReadConfig()
 	if !slices.Contains(config.BaseUrls, "https://geodienste.ch/stac") ||
 		!slices.Contains(config.BaseUrls, "https://data.geo.admin.ch/api/stac/v1") {
 		t.Errorf("Base URLs are not correct")
